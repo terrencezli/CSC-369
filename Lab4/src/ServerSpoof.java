e09f10faa627773a07976f6384267019681f5ac1
Terrence Li <terrencezli@hotmail.com>
2016-01-26 16:30:18 -0800

    stores in logs, completed up to SSR7

--- Lab4/src/ServerSpoof.java
+++ Lab4/src/ServerSpoof.java
@@ -9,9 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,18 +33,20 @@ public class ServerSpoof {
         JSONObject js;
         Scanner s;
         ArrayList<String> list;
+        FileWriter fw = null;
+
+        Logger logger = Logger.getLogger("org.mongodb.driver");  // turn off logging
+        logger.setLevel(Level.OFF);                              // this lets us squash a lot
 
         try {
+            fw = new FileWriter(new File("server_log.txt"));
             t = new JSONTokener(new FileReader(new File(args[0])));
             js = new JSONObject(t);
             c = new MongoClient(js.getString("mongo"));  // connect to server
             db = c.getDatabase(js.getString("database"));
 
-            Logger logger = Logger.getLogger("org.mongodb.driver");  // turn off logging
-            logger.setLevel(Level.OFF);                              // this lets us squash a lot
 
             String collectionName = js.getString("collection");
-            System.out.println("number of Documents:" + db.getCollection(collectionName).count());
 
             // drops collection
             db.getCollection(js.getString("monitor")).drop();
@@ -58,13 +58,15 @@ public class ServerSpoof {
 //            }
 //            s.close();
 
-            System.out.println("===XXXXXXXXXX==========");
+            String printout = "===XXXXXXXXXX==========\n";
             Date date= new Date();
-            System.out.println(new Timestamp(date.getTime()));
-            System.out.println("database name:" + db.getName());
-            System.out.println("collection:" + collectionName);
-            System.out.println("number of Documents:" + db.getCollection(collectionName).count());
-
+            printout += new Timestamp(date.getTime()) + "\n";
+            printout += "database name:" + db.getName() + "\n";
+            printout += "collection:" + collectionName + "\n";
+            printout += "number of Documents:" + db.getCollection(collectionName).count() + "\n";
+            System.out.println(printout);
+            fw.write(printout);
+            fw.flush();
 
             long lastTotal = 0;
             while (true) {
@@ -103,12 +105,12 @@ public class ServerSpoof {
                 AggregateIterable<Document> statusMessage = db.getCollection(collectionName).aggregate(asList(
                         new Document("$group", new Document("_id", "$status").append("count", new Document("$sum", 1)))));
 
-                JSONArray statuses = new JSONArray();
+                final JSONArray statuses = new JSONArray();
                 statusMessage.forEach(new Block<Document>() {
                     @Override
                     public void apply(final Document document) {
                         statuses.put(document.toJson());
-                        System.out.println(document.toJson());
+                        //System.out.println(document.toJson());
                     }
                 });
                 monitor.put("statusStats", statuses);
@@ -117,19 +119,31 @@ public class ServerSpoof {
                 AggregateIterable<Document> destination = db.getCollection(collectionName).aggregate(asList(
                         new Document("$group", new Document("_id", "$recepient").append("count", new Document("$sum", 1)))));
 
-                JSONArray recepients = new JSONArray();
+                final JSONArray recepients = new JSONArray();
                 destination.forEach(new Block<Document>() {
                     @Override
                     public void apply(final Document document) {
                         recepients.put(document.toJson());
-                        System.out.println(document.toJson());
+                        //System.out.println(document.toJson());
                     }
                 });
                 monitor.put("recepientStats", recepients);
+
+                System.out.println(monitor.toString(2));
+                fw.write(monitor.toString(2));
+                fw.flush();
             }
 
-        } catch (FileNotFoundException | InterruptedException | JSONException e) {
+        } catch (InterruptedException | JSONException | IOException e) {
             e.printStackTrace();
+        }
+        finally {
+            try {
+                fw.flush();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
